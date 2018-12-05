ed42da8bb65e913ade3999a1504a767f1055e48b
Dusan Jovanovic <djovanovic@flipboard.com>
2018-12-05 11:26:26 -0500

    add functional solution

--- python/05_alchemical_reduction.py
+++ python/05_alchemical_reduction.py
@@ -6,17 +6,27 @@ input_file = join(dirname(realpath(__file__)), '..', 'inputs', '05.txt')
 
 
 def react_polymer(polymer):
+    # stack-based solution
     stack = []
     for c in polymer:
         if not stack:
             stack.append(c)
             continue
-        last = stack[-1]
-        if last != c and last.lower() == c.lower():
+        if stack[-1] != c and stack[-1].lower() == c.lower():
             stack.pop()
         else:
             stack.append(c)
     return stack
+
+
+def polymer_reduction(acc, right):
+    # functional solution
+    if not acc:
+        return right
+    if acc[-1] != right and acc[-1].lower() == right.lower():
+        return acc[:-1]
+    else:
+        return acc + right
 
 
 def filter_chars(s, exclude_chars):
@@ -27,7 +37,7 @@ def main():
     polymer = open(input_file).readline().strip()
 
     # part 1
-    print len(react_polymer(polymer))
+    print len(react_polymer(polymer)), len(reduce(polymer_reduction, polymer))
 
     # part 2
     pairs = [set(p) for p in zip(ascii_lowercase, ascii_lowercase.upper())]
