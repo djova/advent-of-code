9b0babd7ee701d5520c59e311e6ac7148d061543
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2019-12-01 07:56:18 -0500

    day 1

---
+++ 2019/python/01_The_Tyranny_of_the_Rocket_Equation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import math
+from os.path import join, dirname, realpath
+
+input_file = join(dirname(realpath(__file__)), '..', 'inputs', '01.txt')
+input_ints = [int(l.strip()) for l in open(input_file, 'r').readlines()]
+
+
+def fuel_required(mass):
+    return math.floor(mass / 3) - 2
+
+
+print("Part 1:", sum([fuel_required(m) for m in input_ints]))
+
+
+def total_fuel_required(mass):
+    total = 0
+    fuel = fuel_required(mass)
+    while fuel > 0:
+        total += fuel
+        fuel = fuel_required(fuel)
+    return total
+
+
+print("Part 2:", sum([total_fuel_required(m) for m in input_ints]))
