ac01885344afa159a6f71114de4ffbc332a97136
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2022-12-19 21:22:45 -0800

    day 8

--- 2022/python/day_08.py
+++ 2022/python/day_08.py
@@ -48,6 +48,18 @@ def visible_from_edge(row):
             yield 0
 
 
+def visible_trees(row):
+    row = list(row)
+    for i in range(len(row)):
+        count = 0
+        for j in reversed(range(0, i)):
+            count += 1
+            if row[j] >= row[i]:
+                break
+
+        yield count
+
+
 def count_non_zero(grid):
     result = 0
     for row in grid:
@@ -57,13 +69,13 @@ def count_non_zero(grid):
     return result
 
 
-def merge_grids(grids):
+def merge_grids(grids, merge_func):
     n_rows, n_cols = len(grids[0]), len(grids[0][0])
-    result = [[0 for _ in range(n_cols)] for _ in range(n_rows)]
-    for g in grids:
+    result = grids[0]
+    for g in grids[1:]:
         for i in range(n_rows):
             for j in range(n_cols):
-                result[i][j] += g[i][j]
+                result[i][j] = merge_func(result[i][j], g[i][j])
     return result
 
 
@@ -90,22 +102,25 @@ def count_visible(grid, direction, vfunc):
     return visible
 
 
+sides = ["left", "right", "top", "bottom"]
+
+
 def part1(raw_input):
     grid = parse_grid(raw_input)
-    merged = merge_grids([
-        count_visible(grid, "left", visible_from_edge),
-        count_visible(grid, "right", visible_from_edge),
-        count_visible(grid, "top", visible_from_edge),
-        count_visible(grid, "bottom", visible_from_edge)
-    ])
+    grids = [count_visible(grid, side, visible_from_edge) for side in sides]
+    merged = merge_grids(grids, lambda a, b: a + b)
     return count_non_zero(merged)
 
 
 def part2(raw_input):
-    return 0
+    grid = parse_grid(raw_input)
+    sides = ["left", "right", "top", "bottom"]
+    grids = [count_visible(grid, side, visible_trees) for side in sides]
+    merged = merge_grids(grids, lambda a, b: a * b)
+    return max([max(row) for row in merged])
 
 
-def test_visible_left():
+def test_visible_edge_left():
     grid = parse_grid(test_input)
     expected = """\
     10010
@@ -119,7 +134,7 @@ def test_visible_left():
     assert left == expected, "left"
 
 
-def test_visible_right():
+def test_visible_edge_right():
     grid = parse_grid(test_input)
 
     expected = """\
@@ -134,7 +149,7 @@ def test_visible_right():
     assert right == expected, "right"
 
 
-def test_visible_top():
+def test_visible_edge_top():
     grid = parse_grid(test_input)
     expected = """\
     11111
@@ -149,7 +164,7 @@ def test_visible_top():
     assert top == expected, "top"
 
 
-def test_visible_bottom():
+def test_visible_edge_bottom():
     grid = parse_grid(test_input)
     expected = """\
     00000
@@ -168,10 +183,18 @@ def test_part1():
     assert part1(test_input) == 21
 
 
+def test_visible_trees_left():
+    assert list(visible_trees([3, 0, 3, 7, 3])) == [0, 1, 2, 3, 1]
+    assert list(visible_trees([2, 5, 5, 1, 2])) == [0, 1, 1, 1, 2]
+    assert list(visible_trees([6, 5, 3, 3, 2])) == [0, 1, 1, 1, 1]
+    assert list(visible_trees([3, 3, 5, 4, 9])) == [0, 1, 2, 1, 4]
+    assert list(visible_trees([3, 5, 3, 9, 0])) == [0, 1, 1, 3, 1]
+
+
 def test_part2():
-    assert part2(test_input) == 24933642
+    assert part2(test_input) == 8
 
 
 if __name__ == '__main__':
     print("Part 1: ", part1(input_text))
-    # print("Part 2: ", part2(input_text))
+    print("Part 2: ", part2(input_text))
